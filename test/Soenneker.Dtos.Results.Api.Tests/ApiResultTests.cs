f0918d612a4a22db0653d342fb053f6bd04d123f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:41 -0500

    TUnit conversion

--- test/Soenneker.Dtos.Results.Api.Tests/ApiResultTests.cs
+++ test/Soenneker.Dtos.Results.Api.Tests/ApiResultTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Dtos.Results.Api.Tests;
 
 public sealed class ApiResultTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
